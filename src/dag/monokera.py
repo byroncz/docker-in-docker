5454a885917b4345b1d8c5ff0d4247b948561757
Byron Campo <byron.campo@gmail.com>
2024-02-08 15:22:08 +0000

    Adding DAG

---
+++ src/dag/monokera.py
@@ -0,0 +1,121 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.utils.task_group import TaskGroup
+from docker.types import Mount
+
+
+LOCAL_WORKING_DIRECTORY = '/workspaces/docker-in-docker-2'
+WORKING_DIRECTORY = '/usr/src/app'
+loading_scripts = [
+    'agents.py',
+    'claims.py',
+    'insured.py',
+    'payments.py',
+    'premium.py'
+]
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 2, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=3600),
+}
+
+# dag = DAG(
+#     'monokera_reto',
+#     default_args=default_args,
+#     description='A simple test DAG',
+#     schedule_interval=timedelta(days=1),
+# )
+
+with DAG(
+    'monokera_reto',
+    default_args=default_args,
+    description='Un DAG para cargar datos en paralelo usando DockerOperator',
+    schedule_interval=None,
+) as dag:
+
+
+    start = DummyOperator(
+        task_id='start',
+    )
+
+    cleaning = DockerOperator(
+        task_id='cleaning',
+        image='processor:latest',
+        api_version='auto',
+        auto_remove=True,
+        command=f"python {WORKING_DIRECTORY}/cleaning/cleaning.py",
+        docker_url="unix://var/run/docker.sock", 
+        network_mode="bridge",
+        mounts=[
+            Mount(
+                source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/scripts', 
+                target=WORKING_DIRECTORY,
+                type='bind',
+            ),
+            Mount(
+                source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/data', 
+                target=f'{WORKING_DIRECTORY}/data',
+                type='bind',
+            )
+        ]
+    )
+
+    loading_policy = DockerOperator(
+        task_id='loading_policy',
+        image='processor:latest',
+        api_version='auto',
+        auto_remove=True,
+        command=f"python {WORKING_DIRECTORY}/loading/policy.py",
+        docker_url="unix://var/run/docker.sock", 
+        network_mode="bridge",
+        mounts=[
+            Mount(
+                source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/scripts', 
+                target=WORKING_DIRECTORY,
+                type='bind',
+            ),
+            Mount(
+                source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/data', 
+                target=f'{WORKING_DIRECTORY}/data',
+                type='bind',
+            )
+        ]
+    )
+
+    with TaskGroup("loading") as loading:
+            for script in loading_scripts:
+                current_task = DockerOperator(
+                    task_id=f'{script[:-3]}', 
+                    image='processor:latest',
+                    api_version='auto',
+                    auto_remove=True,
+                    command=f"python {WORKING_DIRECTORY}/loading/{script}",
+                    docker_url="unix://var/run/docker.sock",
+                    network_mode="bridge",
+                    mounts=[
+                        Mount(
+                            source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/scripts',
+                            target=WORKING_DIRECTORY,
+                            type='bind',
+                        ),
+                        Mount(
+                            source=f'{LOCAL_WORKING_DIRECTORY}/monokera/src/data',
+                            target=f'{WORKING_DIRECTORY}/data',
+                            type='bind',
+                        )
+                    ]
+                )
+
+
+    end = DummyOperator(
+        task_id='end'
+    )
+
+start >> cleaning >> loading_policy >> loading >> end
